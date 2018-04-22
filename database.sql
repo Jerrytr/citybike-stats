73531353543ccd18b17275532050cf2ed6ea47e1
JerryTr <traskelin.jerry@gmail.com>
2018-04-22 10:23:02 +0300

    format database

---
+++ database.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS bike_info_db;
+CREATE DATABASE bike_info_db;
+USE bike_info_db;
+
+CREATE TABLE Bikestations (
+    StationID VARCHAR(10) NOT NULL,
+    StationName VARCHAR(255) NOT NULL,
+    StationLon VARCHAR(255) NOT NULL,
+    StationLat VARCHAR(255) NOT NULL,
+    PRIMARY KEY(StationID)
+);
+
+CREATE TABLE Bikestats (
+    StationID VARCHAR(10) NOT NULL,
+    Bikesfree INT,
+    PRIMARY KEY(StationID),
+    FOREIGN KEY(StationID) REFERENCES Bikestations(StationID)
+);
